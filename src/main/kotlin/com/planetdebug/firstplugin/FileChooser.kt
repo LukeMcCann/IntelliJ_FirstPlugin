d537e96ec0730b7fb53b1f544b8fafaa06d4df22
Luke McCann <developer-lukemccann@outlook.com>
2019-11-22 20:17:50 +0000

    Adds basic file path printer (file chooser)

--- src/main/kotlin/com/planetdebug/firstplugin/FileChooser.kt
+++ src/main/kotlin/com/planetdebug/firstplugin/FileChooser.kt
@@ -2,8 +2,10 @@ package com.planetdebug.firstplugin
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.fileChooser.FileChooser
 import com.intellij.openapi.fileChooser.FileChooserDescriptor
 import com.intellij.openapi.ui.Messages
+// import com.intellij.openapi.ui.Messages
 import com.intellij.ui.layout.chooseFile
 
 class FileChooser: AnAction()
@@ -11,5 +13,19 @@ class FileChooser: AnAction()
     override fun actionPerformed(e: AnActionEvent)
     {
         // Messages.showMessageDialog(e.project, "C:file_here", "File Dialog", Messages.getInformationIcon())
+
+        val fileChooserDescriptor = FileChooserDescriptor(
+            false,
+            true,
+            false,
+            false,
+            false,
+            false
+        )
+        fileChooserDescriptor.title = "Select Directory"
+        fileChooserDescriptor.description = "File Chooser"
+        FileChooser.chooseFile(fileChooserDescriptor, e.project, null) {
+            Messages.showMessageDialog(e.project, it.path, "Path", Messages.getInformationIcon())
+        }
     }
 }
